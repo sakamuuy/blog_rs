eaa37b8df686cf7305b7f02448acaee29edfce29
sakamuuy <sakamuu.y@gmail.com>
2023-02-21 20:26:34 +0900

    wip fetch from micro cms

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::collections::HashMap;
+use std::env;
+
 use actix_web::{error, middleware, web, App, Error, HttpResponse, HttpServer};
 use serde::Serialize;
 use tera::Tera;
@@ -9,9 +12,24 @@ struct Article {
     date: String,
 }
 
+
+fn get_from_env(key: &str) -> String {
+    match env::var(key) {
+        Ok(v) => v,
+        Err(_) => "".to_string()
+    }
+}
+
+async fn fetch_from_micro_cms() -> Result<(), Box<dyn std::error::Error>> {
+    let end_point = get_from_env("END_POINT");
+println!("end: {}", end_point);
+    let resp = reqwest::get(end_point + "/api/v1/article").await?.json::<HashMap<String, String>>().await?;
+    println!("{:#?}", resp);
+    Ok(())
+}
+
 async fn index(tmpl: web::Data<Tera>) -> Result<HttpResponse, Error> {
     let mut ctx = tera::Context::new();
-
     let articles = vec![
         Article {
             title: "a1".to_string(),
@@ -36,6 +54,7 @@ async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
 
+fetch_from_micro_cms();
     HttpServer::new(|| {
         let templates = Tera::new("templates/**/*").unwrap();
 
