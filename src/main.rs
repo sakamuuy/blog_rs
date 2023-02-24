b0100e93e47329bfff9309e355d3e4c1dabfbe7c
sakamuuy <sakamuu.y@gmail.com>
2023-02-24 20:30:45 +0900

    Fetch data from micro cms

--- src/main.rs
+++ src/main.rs
@@ -3,43 +3,57 @@ use std::env;
 use dotenv::dotenv;
 
 use actix_web::{error, middleware, web, App, Error, HttpResponse, HttpServer};
-use serde::Serialize;
+use serde::{Serialize, Deserialize};
 use tera::Tera;
 
-#[derive(Serialize)]
-struct Article {
+#[derive(Debug, Serialize, Deserialize)]
+struct Category {
+    id: String,
+    name: String
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Content {
+    id: String,
+    #[serde(rename = "createdAt")]
+    created_at: String,
+    #[serde(rename = "updatedAt")]
+    updated_at: String,
     title: String,
-    url: String,
-    date: String,
+    body: String,
+    category: Category
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct ArticlesFromMicroCMS {
+    contents: Vec<Content>
 }
 
 async fn fetch_from_micro_cms(end_point: &str, api_key: &str) -> Result<(), Box<dyn std::error::Error>> {
     let end_point = end_point.to_string();
     let client = reqwest::Client::new();
-    let res = client.get(end_point + "/api/v1/article").header("X-MICROCMS-API-KEY", api_key).send().await?.json::<HashMap<String, String>>().await;
+    let res: ArticlesFromMicroCMS = client.get(end_point + "/api/v1/article").header("X-MICROCMS-API-KEY", api_key).send().await?.json().await?;
 
-    match res {
-        Ok(v) => println!("{:#?}", v),
-        Err(e) => println!("{}", e)
-    }
+    println!("res: {:#?}", res);
+
     Ok(())
 }
 
 async fn index(tmpl: web::Data<Tera>) -> Result<HttpResponse, Error> {
     let mut ctx = tera::Context::new();
-    let articles = vec![
-        Article {
-            title: "a1".to_string(),
-            url: "".to_string(),
-            date: "2023/02/03".to_string(),
-        },
-        Article {
-            title: "a2".to_string(),
-            url: "".to_string(),
-            date: "2023/02/03".to_string(),
-        },
-    ];
-    ctx.insert("articles", &articles);
+    // let articles = vec![
+    //     Article {
+    //         title: "a1".to_string(),
+    //         url: "".to_string(),
+    //         date: "2023/02/03".to_string(),
+    //     },
+    //     Article {
+    //         title: "a2".to_string(),
+    //         url: "".to_string(),
+    //         date: "2023/02/03".to_string(),
+    //     },
+    // ];
+    // ctx.insert("articles", &articles);
     let view = tmpl
         .render("index.html.tera", &ctx)
         .map_err(|e| error::ErrorInternalServerError(e))?;
