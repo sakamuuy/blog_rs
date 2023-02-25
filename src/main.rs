f198278b3df0b643492def9c41ec32c77d1e487b
sakamuuy <sakamuu.y@gmail.com>
2023-02-25 23:44:21 +0900

    List from micro cms'

--- src/main.rs
+++ src/main.rs
@@ -29,31 +29,25 @@ struct ArticlesFromMicroCMS {
     contents: Vec<Content>
 }
 
-async fn fetch_from_micro_cms(end_point: &str, api_key: &str) -> Result<(), Box<dyn std::error::Error>> {
+async fn fetch_from_micro_cms(end_point: &str, api_key: &str) -> Result<ArticlesFromMicroCMS, Box<dyn std::error::Error>> {
     let end_point = end_point.to_string();
     let client = reqwest::Client::new();
     let res: ArticlesFromMicroCMS = client.get(end_point + "/api/v1/article").header("X-MICROCMS-API-KEY", api_key).send().await?.json().await?;
 
     println!("res: {:#?}", res);
 
-    Ok(())
+    Ok(res)
 }
 
 async fn index(tmpl: web::Data<Tera>) -> Result<HttpResponse, Error> {
     let mut ctx = tera::Context::new();
-    // let articles = vec![
-    //     Article {
-    //         title: "a1".to_string(),
-    //         url: "".to_string(),
-    //         date: "2023/02/03".to_string(),
-    //     },
-    //     Article {
-    //         title: "a2".to_string(),
-    //         url: "".to_string(),
-    //         date: "2023/02/03".to_string(),
-    //     },
-    // ];
-    // ctx.insert("articles", &articles);
+
+    let end_point = env::var("END_POINT").expect("END_POINT must be set");
+    let api_key = env::var("API_KEY").expect("API_KEY must be set");
+    let res = fetch_from_micro_cms(&end_point, &api_key).await?;
+
+    ctx.insert("articles", &res.contents);
+
     let view = tmpl
         .render("index.html.tera", &ctx)
         .map_err(|e| error::ErrorInternalServerError(e))?;
@@ -65,10 +59,6 @@ async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
     dotenv().ok();
-
-    let end_point = env::var("END_POINT").expect("END_POINT must be set");
-    let api_key = env::var("API_KEY").expect("API_KEY must be set");
-    fetch_from_micro_cms(&end_point, &api_key).await;
 
     HttpServer::new(|| {
         let templates = Tera::new("templates/**/*").unwrap();
